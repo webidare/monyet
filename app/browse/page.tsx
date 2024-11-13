8a6bd68855aef7ccd3d0ff204adaac15de36304d
webidare <webidevolution@gmail.com>
2024-11-13 13:09:38 +0700

    Update page.tsx

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState, useEffect } from 'react';
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
